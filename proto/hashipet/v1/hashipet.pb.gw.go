1ae9e1f6d2f80ac3a21a9092b37b262aac7ae0cf
Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>
2022-05-06 14:44:42 -0400

    Upgrade plugin version

--- proto/hashipet/v1/hashipet.pb.gw.go
+++ proto/hashipet/v1/hashipet.pb.gw.go
@@ -199,12 +199,13 @@ func RegisterHashiPetServiceHandlerServer(ctx context.Context, mux *runtime.Serv
 		var stream runtime.ServerTransportStream
 		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/hashipet.v1.HashiPetService/CreatePet", runtime.WithHTTPPathPattern("/v1/pets"))
+		var err error
+		ctx, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/hashipet.v1.HashiPetService/CreatePet", runtime.WithHTTPPathPattern("/v1/pets"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := local_request_HashiPetService_CreatePet_0(rctx, inboundMarshaler, server, req, pathParams)
+		resp, md, err := local_request_HashiPetService_CreatePet_0(ctx, inboundMarshaler, server, req, pathParams)
 		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
@@ -222,12 +223,13 @@ func RegisterHashiPetServiceHandlerServer(ctx context.Context, mux *runtime.Serv
 		var stream runtime.ServerTransportStream
 		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/hashipet.v1.HashiPetService/GetPet", runtime.WithHTTPPathPattern("/v1/pets/{name}"))
+		var err error
+		ctx, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/hashipet.v1.HashiPetService/GetPet", runtime.WithHTTPPathPattern("/v1/pets/{name}"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := local_request_HashiPetService_GetPet_0(rctx, inboundMarshaler, server, req, pathParams)
+		resp, md, err := local_request_HashiPetService_GetPet_0(ctx, inboundMarshaler, server, req, pathParams)
 		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
@@ -245,12 +247,13 @@ func RegisterHashiPetServiceHandlerServer(ctx context.Context, mux *runtime.Serv
 		var stream runtime.ServerTransportStream
 		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/hashipet.v1.HashiPetService/ListPets", runtime.WithHTTPPathPattern("/v1/pets"))
+		var err error
+		ctx, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/hashipet.v1.HashiPetService/ListPets", runtime.WithHTTPPathPattern("/v1/pets"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := local_request_HashiPetService_ListPets_0(rctx, inboundMarshaler, server, req, pathParams)
+		resp, md, err := local_request_HashiPetService_ListPets_0(ctx, inboundMarshaler, server, req, pathParams)
 		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
@@ -268,12 +271,13 @@ func RegisterHashiPetServiceHandlerServer(ctx context.Context, mux *runtime.Serv
 		var stream runtime.ServerTransportStream
 		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/hashipet.v1.HashiPetService/DeletePet", runtime.WithHTTPPathPattern("/v1/pets/{name}"))
+		var err error
+		ctx, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/hashipet.v1.HashiPetService/DeletePet", runtime.WithHTTPPathPattern("/v1/pets/{name}"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := local_request_HashiPetService_DeletePet_0(rctx, inboundMarshaler, server, req, pathParams)
+		resp, md, err := local_request_HashiPetService_DeletePet_0(ctx, inboundMarshaler, server, req, pathParams)
 		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
@@ -330,12 +334,13 @@ func RegisterHashiPetServiceHandlerClient(ctx context.Context, mux *runtime.Serv
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateContext(ctx, mux, req, "/hashipet.v1.HashiPetService/CreatePet", runtime.WithHTTPPathPattern("/v1/pets"))
+		var err error
+		ctx, err = runtime.AnnotateContext(ctx, mux, req, "/hashipet.v1.HashiPetService/CreatePet", runtime.WithHTTPPathPattern("/v1/pets"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := request_HashiPetService_CreatePet_0(rctx, inboundMarshaler, client, req, pathParams)
+		resp, md, err := request_HashiPetService_CreatePet_0(ctx, inboundMarshaler, client, req, pathParams)
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
@@ -350,12 +355,13 @@ func RegisterHashiPetServiceHandlerClient(ctx context.Context, mux *runtime.Serv
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateContext(ctx, mux, req, "/hashipet.v1.HashiPetService/GetPet", runtime.WithHTTPPathPattern("/v1/pets/{name}"))
+		var err error
+		ctx, err = runtime.AnnotateContext(ctx, mux, req, "/hashipet.v1.HashiPetService/GetPet", runtime.WithHTTPPathPattern("/v1/pets/{name}"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := request_HashiPetService_GetPet_0(rctx, inboundMarshaler, client, req, pathParams)
+		resp, md, err := request_HashiPetService_GetPet_0(ctx, inboundMarshaler, client, req, pathParams)
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
@@ -370,12 +376,13 @@ func RegisterHashiPetServiceHandlerClient(ctx context.Context, mux *runtime.Serv
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateContext(ctx, mux, req, "/hashipet.v1.HashiPetService/ListPets", runtime.WithHTTPPathPattern("/v1/pets"))
+		var err error
+		ctx, err = runtime.AnnotateContext(ctx, mux, req, "/hashipet.v1.HashiPetService/ListPets", runtime.WithHTTPPathPattern("/v1/pets"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := request_HashiPetService_ListPets_0(rctx, inboundMarshaler, client, req, pathParams)
+		resp, md, err := request_HashiPetService_ListPets_0(ctx, inboundMarshaler, client, req, pathParams)
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
@@ -390,12 +397,13 @@ func RegisterHashiPetServiceHandlerClient(ctx context.Context, mux *runtime.Serv
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateContext(ctx, mux, req, "/hashipet.v1.HashiPetService/DeletePet", runtime.WithHTTPPathPattern("/v1/pets/{name}"))
+		var err error
+		ctx, err = runtime.AnnotateContext(ctx, mux, req, "/hashipet.v1.HashiPetService/DeletePet", runtime.WithHTTPPathPattern("/v1/pets/{name}"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := request_HashiPetService_DeletePet_0(rctx, inboundMarshaler, client, req, pathParams)
+		resp, md, err := request_HashiPetService_DeletePet_0(ctx, inboundMarshaler, client, req, pathParams)
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
